8abc7c5c8e7493bd91a51b7a4ba4a6119f3b2dcc
Alex Britt <alexxbritt@gmail.com>
2014-10-28 15:52:33 -0400

    made function to show hide divs easily
    function showDiv(which)
    - which is "home", "quiz", or "sandbox"
    
    also added some missing semicolons

--- Home.js
+++ Home.js
@@ -1,31 +1,42 @@
 $(document).ready(function(){
 	$("#homeSubmit").click(function(event){
-		 var checked=($('input[name=song]:checked').is(':checked'))
-		 if(checked==false)
-		 {	
+		 var checked=($('input[name=song]:checked').is(':checked'));
+		 if(checked==false) {	
 		 	alert("Song Not Selected");
 		 }
-		 var checked2=($('input[name=level]:checked').is(':checked'))
-		 if(checked2==false){
+		 var checked2=($('input[name=level]:checked').is(':checked'));
+		 if(checked2==false) {
 		 	alert("Level not selected");
-		}
-		else{
-			$("#homeDiv").attr("divIsVisible", "false");
-			$("#quizDiv").attr("divIsVisible", "true");
-			$("#sandboxDiv").attr("divIsVisible", "false");
-			var names=($('input[name=song]:checked').val())
+		} else {
+			showDiv("quiz");
+			var names=($('input[name=song]:checked').val());
 			alert(names);
 			tones=createAudios(names, 1, 1);
 		}
 	});
 
 	$("#headerDivTitle").on("click", function(event) {
-		$("#homeDiv").attr("divIsVisible", "true");
-		$("#quizDiv").attr("divIsVisible", "false");
-		$("#sandboxDiv").attr("divIsVisible", "false");
+		showDiv("home");
 	});
 });
 
+function showDiv(which) {
+	if (which=="home") {
+		$("#homeDiv").attr("divIsVisible", "true");
+		$("#quizDiv").attr("divIsVisible", "false");
+		$("#sandboxDiv").attr("divIsVisible", "false");
+	}
+	if (which=="quiz") {
+		$("#homeDiv").attr("divIsVisible", "false");
+		$("#quizDiv").attr("divIsVisible", "true");
+		$("#sandboxDiv").attr("divIsVisible", "false");
+	}
+	if (which=="sandbox") {
+		$("#homeDiv").attr("divIsVisible", "false");
+		$("#quizDiv").attr("divIsVisible", "false");
+		$("#sandboxDiv").attr("divIsVisible", "true");
+	}
+}
 // function that does this parametrically
 // $("#homeDiv").attr("divIsVisible", "true");
 // 		$("#quizDiv").attr("divIsVisible", "false");
